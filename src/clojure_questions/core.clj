a29a0fd0aef30638c6c515d1a7bfae60a3d16c30
Anthony Grimes <i@raynes.me>
2012-02-25 04:19:29 -0600

    We can has questions.

--- src/clojure_questions/core.clj
+++ src/clojure_questions/core.clj
@@ -1,6 +1,8 @@
 (ns clojure-questions.core
   (:require [twitter.oauth :as to]
-            [twitter.api.restful :as tr]))
+            [twitter.api.restful :as tr]
+            [clj-http.client :as http]
+            [cheshire.core :as json]))
 
 (def creds
   (let [config (read-string (slurp "creds.clj"))]
@@ -12,3 +14,13 @@
 (defn tweet [msg]
   (tr/update-status :oauth-creds creds
                     :params {:status msg}))
+
+(def so "http://api.stackoverflow.com/1.1/")
+
+(defn questions [min-date]
+  (json/parse-string
+    (:body
+      (http/get (str so "questions")
+                {:query-params {:tagged "clojure"
+                                :fromdate min-date
+                                :pagesize 3}}))))
