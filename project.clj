e08d62fb2653e1d8a31f19e7ca3a151c1b813fee
Anthony Grimes <i@raynes.me>
2012-02-25 21:31:04 -0600

    We have a bot.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.3.2"]
                  [cheshire "2.2.0"]
-                 [twitter-api "0.6.4"]])
+                 [twitter-api "0.6.4"]
+                 [hobbit "0.1.0-alpha1"]]
+  :main clojure-questions.core)
--- src/clojure_questions/core.clj
+++ src/clojure_questions/core.clj
@@ -2,7 +2,11 @@
   (:require [twitter.oauth :as to]
             [twitter.api.restful :as tr]
             [clj-http.client :as http]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [hobbit.isgd :as isgd]
+            [hobbit.core :as hobbit]
+            [clojure.string :as string])
+  (:gen-class))
 
 (def creds
   (let [config (read-string (slurp "creds.clj"))]
@@ -17,10 +21,34 @@
 
 (def so "http://api.stackoverflow.com/1.1/")
 
+(defn now [] (quot (System/currentTimeMillis) 1000))
+
+(defn compose-tweets [questions]
+  (for [{:strs [title question_id]} questions
+        :let [link (hobbit/shorten
+                     (isgd/isgd-shortener)
+                     (str "http://stackoverflow.com/questions/" question_id))
+              link-count (count link)
+              title-count (count title)]]
+    (if (>= 140 (doto (+ title-count link-count 1) prn))
+      (str title \space link)
+      (str (string/join (take (- 140 (+ link-count 2)) title))
+           \u2026 \space link))))
+
 (defn questions [min-date]
-  (json/parse-string
-    (:body
-      (http/get (str so "questions")
-                {:query-params {:tagged "clojure"
-                                :fromdate min-date
-                                :pagesize 3}}))))
+  (compose-tweets
+    ((json/parse-string
+       (:body
+         (http/get (str so "questions")
+                   {:query-params {:tagged "clojure"
+                                   :fromdate min-date}})))
+       "questions")))
+
+(defn -main [& _]
+  (loop [time (now)]
+    (println "Checking for new questions.")
+    (doseq [message (questions time)]
+      (println "Tweeting:" message)
+      (tweet message))
+    (Thread/sleep 300000)
+    (recur (now))))
